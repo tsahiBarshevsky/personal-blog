7af178110fbf5bddc1cfb1b4426dd47353af8f88
tsahiBarshevsky <tsahi.13@gmail.com>
2021-02-22 16:20:23 +0200

    Getting images from storage

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -21,12 +21,16 @@ const theme = createMuiTheme({
 export default function Post(props) 
 {
     const [post, setPost] = useState({});
+    const [images, setImages] = useState([]);
+    console.log(images);
     const [fault, setFault] = useState(false);
     const title = props.match.params.title;
     const image = "https://firebasestorage.googleapis.com/v0/b/tsahis-website.appspot.com/o/Backgrounds%2FIMG_0561_Easy-Resize.com.jpg?alt=media&token=f6d4acc4-f5ea-41c1-b018-e3829afeac08";
     const background = {background: `url(${post.mainImageLink}) fixed center center`};
 
-    useEffect(() => {
+    useEffect(() => 
+    {
+        // get post values
         firebase.db.collection("posts").doc(title).get().then(function(doc)
         {
             if (doc.exists)
@@ -34,6 +38,19 @@ export default function Post(props)
             else
                 setFault(true);
         });
+        
+        //get post images' links
+        const storageRef = firebase.storage.ref();
+        storageRef.child(`posts/${title}`).listAll()
+        .then((res) => {
+            res.items.forEach((itemRef) => {
+                itemRef.getDownloadURL().then(url => images.push(url));
+            });
+        }).catch((error) => {
+            console.log(error.message);
+        });
+
+        // disable right click
         document.addEventListener('contextmenu', (e) => 
         {
             e.preventDefault();
@@ -43,7 +60,7 @@ export default function Post(props)
     const renderPost = () =>
     {
         //var paragraphs = post.text.split("\n");
-        var paragraphs = ["p0", "p1", "p2", "", "p3", "p4", "", "p5", "p6", "", "p7", "p8"];
+        var paragraphs = ["p1", "p2", "", "p3", "p4", "", "p5", "p6"];
         var img = ["img1", "img2", "img3"];
         var i = 0;
         return (paragraphs.map((paragraph, index) =>
@@ -55,8 +72,8 @@ export default function Post(props)
                     </div>
                 : 
                     <div className="paragraphs">
-                        <p>{img[i++]}</p>
-                        {/*<img src={post.images[i]} style={{ width: 500 }} />*/}
+                        {/*<p>{img[i++]}</p>*/}
+                        <img src={images[i++]} style={{ width: 500 }} />
                         <br />
                     </div>}
             </div>
